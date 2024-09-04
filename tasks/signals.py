1c59e41fd9f69e7952dff090712c84cb655c6f19
Reyner <reyenr1012002@gmail.com>
2024-09-04 15:20:56 -0600

    AWS S3 CONFIGURED

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,17 +1,17 @@
-import os
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
+from django.core.files.storage import default_storage
 from .models import Task, TaskCompletedProof
 
 @receiver(post_delete, sender=Task)
 def delete_file(sender, instance, **kwargs):
     if instance.help_image:
-        if os.path.isfile(instance.help_image.path):
-            os.remove(instance.help_image.path)
+        if default_storage.exists(instance.help_image.name):
+            instance.help_image.delete(save=False)
 
 
 @receiver(post_delete, sender=TaskCompletedProof)
 def delete_proof_file(sender, instance, **kwargs):
     if instance.proof_image:
-        if os.path.isfile(instance.proof_image.path):
-            os.remove(instance.proof_image.path)
+        if default_storage.exists(instance.proof_image.name):
+            instance.proof_image.delete(save=False)
