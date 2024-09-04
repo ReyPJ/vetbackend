57b9159cb9db6ae101d0f7c850917634a463eb5c
Reyner <reyenr1012002@gmail.com>
2024-09-03 19:33:43 -0600

    Trabajando en deploy

--- backend/settings.py
+++ backend/settings.py
@@ -79,11 +79,13 @@ INSTALLED_APPS = [
 ]
 
 # Digital Ocean Spaces
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS')
 AWS_ACCESS_SECRET_KEY = os.getenv('AWS_SECRET')
 AWS_STORAGE_BUCKET_NAME = 'django-assets'
 AWS_S3_ENDPOINT_URL = 'https://django-assets.nyc3.digitaloceanspaces.com'
+AWS_S3_REGION_NAME = 'nyc3'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.nyc3.digitaloceanspaces.com'
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
@@ -182,12 +184,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = f"{AWS_S3_ENDPOINT_URL}/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = f"{AWS_S3_ENDPOINT_URL}/media/"
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
 
 # Default primary key field type
